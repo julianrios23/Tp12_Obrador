09a14b5daa30c3ebe4a081da852b049dfd591afa
Julian Rios <julianelsensei@gmail.com>
2023-10-03 18:37:40 -0300

    Initial commit

---
+++ src/construirsa/ConstruirSA.java
@@ -0,0 +1,60 @@
+
+
+package construirsa;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Julian Development
+ */
+public class ConstruirSA {
+
+    
+    public static void main(String[] args) {
+        
+         Connection con;
+        con = Conexion.getConexion();
+        
+//        String sql = "INSERT INTO empleado (dni, apellido, nombre, acceso, estado) VALUES (?, ?, ?, ?, ?)";
+//        
+//        try (PreparedStatement ps = con.prepareStatement(sql)) {
+//            //empleado 1
+//            int dni = 30200666;
+//            String apellido = "Torrez";
+//            String nombre = "Gabriel";
+//            int acc = 1;
+//            boolean estado = true;
+//
+//            ps.setInt(1, dni);
+//            ps.setString(2, apellido);
+//            ps.setString(3, nombre);
+//            ps.setInt(4, acc);
+//            ps.setBoolean(5, estado);
+//            ps.executeUpdate();
+//            
+//
+//            System.out.println("Se cargo el empleado correctamenet");
+//        } catch (SQLException e) {
+//            System.err.println("Error al cargar datos: " + e.getMessage());
+//        }
+        
+        String sql1 = "INSERT INTO herramientas (nombre, descripcion, stock, estado) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement ps = con.prepareStatement(sql1)) {
+            //  materia
+            ps.setString(1, "Amoladora");
+            ps.setString(2, "De mano");
+            ps.setInt(3, 11); 
+            ps.setBoolean(4, true); 
+            ps.executeUpdate();
+
+            System.out.println("Se cargo la herramienta.");
+        } catch (SQLException e) {
+            System.err.println("Error al cargar datos: " + e.getMessage());
+
+        }
+        
+    }
+
+}
