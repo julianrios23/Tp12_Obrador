c3954ef67b1d44264637650959edadd9063088db
mateosbodio <141843461+mateosbodio@users.noreply.github.com>
2023-10-05 17:28:50 -0300

    tpterminado

--- src/construirsa/ConstruirSA.java
+++ src/construirsa/ConstruirSA.java
@@ -69,21 +69,74 @@ public class ConstruirSA {
             System.err.println("Error al cargar datos: " + e.getMessage());
         }
 
-//        String sql1 = "INSERT INTO herramientas (nombre, descripcion, stock, estado) VALUES (?, ?, ?, ?)";
-//
-//        try (PreparedStatement ps = con.prepareStatement(sql1)) {
-//            //  herramienta
-//            ps.setString(1, "Amoladora");
-//            ps.setString(2, "De mano");
-//            ps.setInt(3, 11); 
-//            ps.setBoolean(4, true); 
-//            ps.executeUpdate();
-//
-//            System.out.println("Se cargo la herramienta.");
-//        } catch (SQLException e) {
-//            System.err.println("Error al cargar datos: " + e.getMessage());
-//
-//        }
+        String sql1 = "INSERT INTO herramientas (nombre, descripcion, stock, estado) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement ps = con.prepareStatement(sql1)) {
+            //  herramienta
+            ps.setString(1, "Amoladora");
+            ps.setString(2, "De mano");
+            ps.setInt(3, 11); 
+            ps.setBoolean(4, true); 
+            ps.executeUpdate();
+            try (PreparedStatement ps1 = con.prepareStatement(sql1)){
+                  ps1.setString(1, "Taladro");
+            ps1.setString(2, "a bateria");
+            ps1.setInt(3, 20); 
+            ps1.setBoolean(4, true); 
+            ps1.executeUpdate();
+            
+            } catch (SQLException e){
+                    
+            } 
+             try (PreparedStatement ps2 = con.prepareStatement(sql1)){
+                  ps2.setString(1, "Martillo");
+            ps2.setString(2, "neumatico");
+            ps2.setInt(3, 15); 
+            ps2.setBoolean(4, true); 
+            ps2.executeUpdate();
+            
+            } catch (SQLException e){
+                    
+            }  
+            
+        
+            
+            System.out.println("Se cargo la herramienta.");
+        } catch (SQLException e) {
+            System.err.println("Error al cargar datos: " + e.getMessage());
+
+        }
+        //metodo listar herramientas
+          String consulta = "SELECT * FROM herramientas WHERE stock >= 10";
+
+        try (PreparedStatement ps = con.prepareStatement(consulta); 
+                ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                int id_herramienta =rs.getInt("id_herramienta");
+                String nombre=rs.getString("nombre");
+                String descripcion=rs.getString("descripcion");
+                int stock=rs.getInt("stock");
+                boolean estado=rs.getBoolean("estado");
+                
+                        
+                System.out.println("nombre: "+nombre+" descripcion: "+descripcion+" stock: "+stock);
+            }
+        } catch (SQLException e) {
+            System.out.println("error"+e.getMessage());
+        }
+
+       //metodo dar de baja 1er empleado.
+        
+            String cons = "DELETE FROM empleado WHERE id_empleado=1";
+           try (PreparedStatement p3 = con.prepareStatement(cons)){
+                    
+                    ResultSet rs= p3.executeQuery();
+                    System.out.println("el emplado se elimino correctamente.");
+                    } catch (SQLException e){
+                         System.err.println("Error: " + e.getMessage());
+                    }
+    
     }
 
 }
