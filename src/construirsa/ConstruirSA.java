71847473b54560be23058b0b13d690f74271f1fd
Julian Rios <julianelsensei@gmail.com>
2023-10-05 14:42:11 -0300

    actualizacion metodo empleado

--- src/construirsa/ConstruirSA.java
+++ src/construirsa/ConstruirSA.java
@@ -15,20 +15,23 @@ public class ConstruirSA {
 
         String sql = "INSERT INTO empleado (dni, apellido, nombre, acceso, estado) VALUES (?, ?, ?, ?, ?)";
 
-        try (PreparedStatement ps = con.prepareStatement(sql)) {
+        try {
             //empleado 1
             int dni = 21450;
             String apellido = "Perez";
             String nombre = "Juan";
             int acc = 1;
             boolean estado = true;
+           
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setInt(1, dni);
+                ps.setString(2, apellido);
+                ps.setString(3, nombre);
+                ps.setInt(4, acc);
+                ps.setBoolean(5, estado);
+                ps.executeUpdate();
+            }
 
-            ps.setInt(1, dni);
-            ps.setString(2, apellido);
-            ps.setString(3, nombre);
-            ps.setInt(4, acc);
-            ps.setBoolean(5, estado);
-            ps.executeUpdate();
             //empleado2
             int dni1 = 30256145;
             String apellido1 = "Torrez";
@@ -36,12 +39,15 @@ public class ConstruirSA {
             int acc1 = 1;
             boolean estado1 = true;
 
-            ps.setInt(1, dni);
-            ps.setString(2, apellido);
-            ps.setString(3, nombre);
-            ps.setInt(4, acc);
-            ps.setBoolean(5, estado);
-            ps.executeUpdate();
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setInt(1, dni1);
+                ps.setString(2, apellido1);
+                ps.setString(3, nombre1);
+                ps.setInt(4, acc1);
+                ps.setBoolean(5, estado1);
+                ps.executeUpdate();
+            }
+
             //empleado3
             int dni2 = 25037238;
             String apellido2 = "Rios";
@@ -49,15 +55,16 @@ public class ConstruirSA {
             int acc2 = 1;
             boolean estado2 = true;
 
-            ps.setInt(1, dni);
-            ps.setString(2, apellido);
-            ps.setString(3, nombre);
-            ps.setInt(4, acc);
-            ps.setBoolean(5, estado);
-            ps.executeUpdate();
-            
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setInt(1, dni2);
+                ps.setString(2, apellido2);
+                ps.setString(3, nombre2);
+                ps.setInt(4, acc2);
+                ps.setBoolean(5, estado2);
+                ps.executeUpdate();
+            }
 
-            System.out.println("Se cargaron los empleados correctamenet");
+            System.out.println("Se cargaron los empleados correctamente");
         } catch (SQLException e) {
             System.err.println("Error al cargar datos: " + e.getMessage());
         }
